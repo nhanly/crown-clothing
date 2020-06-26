d0faa6b262a99b78b1e41c8acf80149b29c7dfbc
Ly Thien Nhan <nhan.lythien@axonactive.com>
2020-06-26 10:39:35 +0700

    Updating with shop name, collection preview and item

--- src/App.js
+++ src/App.js
@@ -4,22 +4,23 @@ import "./App.css";
 import { Switch, Route } from "react-router-dom";
 
 import HomePage from "./pages/homepage/homepage.component";
+import ShopPage from "./pages/shop/shop.component";
 
 const HatsPage = () => (
-    <div>
-        <h1>HATS Page</h1>
-    </div>
+  <div>
+    <h1>HATS Page</h1>
+  </div>
 );
 
 function App() {
-    return (
-        <div>
-            <Switch>
-                <Route exact path="/" component={HomePage}></Route>
-                <Route path="/hats" component={HatsPage}></Route>
-            </Switch>
-        </div>
-    );
+  return (
+    <div>
+      <Switch>
+        <Route exact path="/" component={HomePage}></Route>
+        <Route path="/shop" component={ShopPage}></Route>
+      </Switch>
+    </div>
+  );
 }
 
 export default App;
