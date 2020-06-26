b39b0c45bf77c94b9f13a3231d455e7d014026fd
Ly Thien Nhan <nhan.lythien@axonactive.com>
2020-06-26 11:58:35 +0700

    Add form input components and styling

--- src/App.js
+++ src/App.js
@@ -6,6 +6,7 @@ import { Switch, Route } from "react-router-dom";
 import HomePage from "./pages/homepage/homepage.component";
 import ShopPage from "./pages/shop/shop.component";
 import Header from "./components/header/header.component";
+import SignInAndSignUp from "./pages/signin-and-signup/signin-and-signup.component";
 
 function App() {
   return (
@@ -14,6 +15,7 @@ function App() {
       <Switch>
         <Route exact path="/" component={HomePage}></Route>
         <Route path="/shop" component={ShopPage}></Route>
+        <Route path="/signin" component={SignInAndSignUp}></Route>
       </Switch>
     </div>
   );
