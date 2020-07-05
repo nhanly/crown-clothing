bb7179b55f1ceb37253065162a608d5672658f27
lee <lee@soxes.ch>
2020-07-05 12:21:08 +0700

    Add redux and implement user reducer and user action to header and app components

--- src/index.js
+++ src/index.js
@@ -1,16 +1,21 @@
 import React from "react";
+import { BrowserRouter } from "react-router-dom";
 import ReactDOM from "react-dom";
+import * as serviceWorker from "./serviceWorker";
+import { Provider } from "react-redux";
+
 import "./index.css";
 import App from "./App";
-import * as serviceWorker from "./serviceWorker";
 
-import { BrowserRouter } from "react-router-dom";
+import store from "./redux/store";
 
 ReactDOM.render(
     <React.StrictMode>
-        <BrowserRouter>
-            <App />
-        </BrowserRouter>
+        <Provider store={store}>
+            <BrowserRouter>
+                <App />
+            </BrowserRouter>
+        </Provider>
     </React.StrictMode>,
     document.getElementById("root")
 );
